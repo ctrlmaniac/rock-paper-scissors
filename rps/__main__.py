13747a1c6f49f7def566c8c603b923eb3e02c06d
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-16 22:38:44 +0200

    finished

--- rps/__main__.py
+++ rps/__main__.py
@@ -1,7 +1,19 @@
 from __future__ import absolute_import
 
-from rps import Game, TheRock, TheMimic, TheNonStrategicOne, TheCaothicOne
 import random
+import time
+
+from rps import (
+    Game,
+    TheCaothicOne,
+    TheMimic,
+    TheNonStrategicOne,
+    TheRock,
+    computers_battle,
+    human_vs_computer,
+    print_pause,
+    validate_input,
+)
 
 players = [
     TheRock(),
@@ -10,8 +22,13 @@ players = [
     TheCaothicOne(),
 ]
 
-player1 = players[random.randint(0, 3)]
-player2 = players[random.randint(0, 3)]
+print_pause("Welcome to Rock Paper Scissors!\n")
+print_pause("1. Do you want to play?")
+print_pause("2. Do you want to see other playing?")
 
-game = Game(player1, player2)
-game.play_game()
+choice = validate_input("Type 1 o 2: ", ["1", "2"])
+
+if int(choice) == 1:
+    human_vs_computer(players)
+else:
+    computers_battle(players)
--- rps/rps.py
+++ rps/rps.py
@@ -2,6 +2,7 @@
 
 import random
 import time
+from unicodedata import name
 
 moves = ["rock", "paper", "scissors"]
 
@@ -43,28 +44,16 @@ class Player:
 
 
 class TheRock(Player):
-    def __init__(self):
-        super().__init__()
-        self.name = "The Rock"
-
     def move(self):
         return "rock"
 
 
 class TheCaothicOne(Player):
-    def __init__(self):
-        super().__init__()
-        self.name = "The Chaotic One"
-
     def move(self):
         return random.choice(moves)
 
 
 class TheMimic(Player):
-    def __init__(self):
-        super().__init__()
-        self.name = "The Mimic"
-
     def learn(self, my_move, their_move):
         self.my_moves.append(my_move)
         self.their_moves.append(their_move)
@@ -80,7 +69,6 @@ class TheNonStrategicOne(Player):
     def __init__(self):
         super().__init__()
         self.my_move_index = random.randint(0, 3)
-        self.name = "The Non-Strategic One"
 
     def move(self):
         if self.my_move_index == 0:
@@ -94,33 +82,78 @@ class TheNonStrategicOne(Player):
             return moves[2]
 
 
+class Human(Player):
+    def move(self):
+        choice = validate_input("Your move: rock, paper or scissors? ", moves)
+
+        return choice
+
+
 class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
 
+        self.p1_score = 0
+        self.p2_score = 0
+
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
-        print_pause(f"{self.p1.name}: {move1}  {self.p2.name}: {move2}\n")
+        print_pause(f"Player One: {move1} - Player Two: {move2}")
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
 
-    def play_game(self):
-        print_pause("\n" "===========" "Game start!" "===========" "\n")
+        if move1 != move2:
+            if beats(move1, move2):
+                self.p1_score += 1
+            else:
+                self.p2_score += 1
 
-        for round in range(10):
+        print_pause(
+            f"Score: Player One {self.p1_score}, "
+            + f"Player Two {self.p2_score}\n",
+            3,
+        )
+
+    def winner(self):
+        winner = (
+            "Player One" if self.p1_score > self.p2_score else "Player Two"
+        )
+
+        print(f"The winner is: {winner}!")
+
+    def play_game(self):
+        print_pause("\n===========\nGame start!\n===========\n")
+
+        round = 0
+        while True:
+            round += 1
             print_pause(f"Round {round}:")
             self.play_round()
 
-        print_pause("Game over!")
+            if (self.p1_score == 3 or self.p2_score == 3) and (
+                (self.p1_score > self.p2_score)
+                or (self.p2_score > self.p1_score)
+            ):
+                break
+
+        self.winner()
 
 
 def computers_battle(players):
     player1 = players[random.randint(0, 3)]
     player2 = players[random.randint(0, 3)]
 
-    game = Game(players[2], player2)
+    game = Game(player1, player2)
+    game.play_game()
+
+
+def human_vs_computer(players):
+    player1 = Human()
+    player2 = players[random.randint(0, 3)]
+
+    game = Game(player1, player2)
     game.play_game()
 
 
@@ -132,14 +165,13 @@ if __name__ == "__main__":
         TheCaothicOne(),
     ]
 
-    # print_pause("Welcome to Rock Paper Scissors!\n")
-    # print_pause("1. Do you want to play?")
-    # print_pause("2. Do you want to see other playing?")
+    print_pause("Welcome to Rock Paper Scissors!\n")
+    print_pause("1. Do you want to play?")
+    print_pause("2. Do you want to see other playing?")
 
-    # choice = validate_input("Type 1 o 2: ", ["1", "2"])
+    choice = validate_input("Type 1 o 2: ", ["1", "2"])
 
-    # if int(choice) == 1:
-    #     pass
-    # else:
-    #     computers_battle(players)
-    computers_battle(players)
+    if int(choice) == 1:
+        human_vs_computer(players)
+    else:
+        computers_battle(players)
